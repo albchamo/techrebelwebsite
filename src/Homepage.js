2506c8d78de39494e2562fde704846ff01d74a7b
albchamo <albchamo@gmail.com>
2023-07-12 19:52:45 -0300

    Social Links, removed cursor

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,6 +3,15 @@ import './Homepage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Homepage = () => {
+    const handleSquareClick = (url) => {
+        window.open(url, '_blank');
+      };
+
+    const handleEmailClick = () => {
+        alert('Contact me at alberto@albertochaves.com');
+      };
+
+
     return (
         <div className="container-fluid p-0">
             <div className="row m-0">
@@ -43,19 +52,24 @@ const Homepage = () => {
             </div>
             <div className="row m-0">
                 <div className="col-12 col-md-4 p-0">
-                    <div className="square">
+                    <div className="square" onClick={() => handleSquareClick('https://twitter.com/TechRebelWorld')}>
                         <h2 className='smaller-text'> 
-                    
-                         Contacto 
+                         Twitter 
                         </h2>
                     </div>
                 </div>
                 <div className="col-12 col-md-4 p-0">
-                    <div className="square">   
+                    <div className="square" onClick={handleEmailClick} > 
+                        <h2 className='smaller-text'> 
+                         Email 
+                        </h2>  
                     </div>
                 </div>
                 <div className="col-12 col-md-4 p-0">
-                    <div className="square">
+                    <div className="square" onClick={() => handleSquareClick('https://www.linkedin.com/in/alberto-chaves-costarica/')}>
+                        <h2 className='smaller-text'> 
+                         Linkedin
+                        </h2> 
                     </div>
                 </div>
             </div>
