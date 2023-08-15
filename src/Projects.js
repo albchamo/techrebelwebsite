bd5572a0508209eab5bd15ca40310f209f7f51c0
albchamo <albchamo@gmail.com>
2023-08-15 15:26:21 -0300

    projects page

--- src/Projects.js
+++ src/Projects.js
@@ -1,44 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import ProjectCard from './components/ProjectCard';
 import './Projects.css';
 import Navbar from './components/Navbar';
 import SocialLinks from './components/SocialLinks';
+import projects from './projectsData';  // <-- Importing the projects data
 
 const ProjectsPage = () => {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await axios.get(
-          'https://api.airtable.com/v0/appBHWZIhC3HrzOcx/Projects',
-          {
-            headers: {
-              Authorization: 'Bearer pateLOnFo5Ktsjgom.c0253d62fee577a813328ad88801c7b75626cf769e3a8cbb2df4abb2f72bd2ae',
-            },
-          }
-        );
-
-        setProjects(response.data.records);
-      } catch (error) {
-        console.error('Error fetching projects:', error);
-      }
-    };
-
-    fetchProjects();
-  }, []);
-
   return (
     <div className="projects-page">
-        <header>
+      <header>
         <Navbar />
-        </header>
+      </header>
       {projects.map((project) => (
         <ProjectCard key={project.id} project={project} />
       ))}
-
-<SocialLinks />
+      <SocialLinks />
     </div>
   );
 };
---
+++ src/projectsData.js
@@ -0,0 +1,30 @@
+// src/projectsData.js
+
+const projects = [
+    {
+      id: '1',
+      fields: {
+        Name: 'Whipz',
+        Description: 'This is a POC that aims to experiment with XRPL NFTs for traceability',
+        Stakeholder: 'Brashlabs',
+        Timeframe: '3 months',
+        TechStack: 'XRP Ledger',
+        Status: 'Active'
+      },
+    },
+    {
+        id: '2',
+      fields: {
+        Name: 'Whipz 2',
+        Description: 'This is a POC that aims to experiment with XRPL NFTs for traceability',
+        Stakeholder: 'Brashlabs',
+        Timeframe: '3 months',
+        TechStack: 'XRP Ledger',
+        Status: 'Active'
+      }
+    },
+    // ... add more projects as needed
+  ];
+  
+  export default projects;
+  
